480069bb9b3f7e35a3f56c9da8845d4a7b62d586
krish-98 <muralikrishnanm98@gmail.com>
2023-12-24 11:57:58 +0530

    minor typo fixed

--- index.js
+++ index.js
@@ -1,32 +1,33 @@
-const express = require("express")
-const app = express()
-const cors = require("cors")
+const express = require('express')
+const cors = require('cors')
 
-require("dotenv").config()
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
+const app = express()
+
+require('dotenv').config()
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 
 app.use(cors())
-app.use(express.static("public"))
+app.use(express.static('public'))
 app.use(express.json())
 
 const PORT = process.env.PORT || 3000
 
-app.get("/", (req, res) => {
-  res.send("Hola from server!")
+app.get('/', (req, res) => {
+  res.send('Hola from server!')
 })
 
-app.post("/checkout-payment", async (req, res) => {
+app.post('/checkout-payment', async (req, res) => {
   try {
     const session = await stripe.checkout.sessions.create({
-      submit_type: "pay",
-      mode: "payment",
-      payment_method_types: ["card"],
-      billing_address_collection: "auto",
-      shipping_options: [{ shipping_rate: "shr_1NeFaFSBzzrld9LFwwmTon9O" }],
+      submit_type: 'pay',
+      mode: 'payment',
+      payment_method_types: ['card'],
+      billing_address_collection: 'auto',
+      shipping_options: [{ shipping_rate: 'shr_1NeFaFSBzzrld9LFwwmTon9O' }],
       line_items: req?.body?.map((item) => {
         return {
           price_data: {
-            currency: "inr",
+            currency: 'inr',
             product_data: {
               name: item.name,
               images: [`${process.env.CLOUDINARY_URL}${item.imageId}`],
