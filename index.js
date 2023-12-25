9298ddbec0859a95415bb61cc2ce8303b85f594e
krish-98 <muralikrishnanm98@gmail.com>
2023-12-25 12:03:46 +0530

    created 2 proxy middlewares

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
 const express = require('express')
 const cors = require('cors')
+const { createProxyMiddleware } = require('http-proxy-middleware')
 
 const app = express()
 
 require('dotenv').config()
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 
-app.use(cors())
+const corsOptions = {
+  origin: '*',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  optionsSuccessStatus: 204,
+}
+
+app.use(cors(corsOptions))
+
+// app.use(cors())
 app.use(express.static('public'))
 app.use(express.json())
 
 const PORT = process.env.PORT || 3000
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
+  console.log(req)
   res.send('Hola from server!')
 })
 
@@ -50,5 +61,37 @@ app.post('/checkout-payment', async (req, res) => {
     res.status(err.statusCode || 500).json(err.message)
   }
 })
+
+app.use(
+  '/api/restaurants',
+  createProxyMiddleware({
+    target: 'https://www.swiggy.com',
+    changeOrigin: true,
+    pathRewrite: {
+      '^/api/restaurants':
+        '/dapi/restaurants/list/v5?lat=9.928668&lng=78.092783&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING',
+    },
+  })
+)
+
+app.use(
+  '/api/restaurant/menu',
+  createProxyMiddleware({
+    target: 'https://www.swiggy.com',
+    changeOrigin: true,
+    pathRewrite: {
+      '^/api/restaurant/menu': '/dapi/menu/pl',
+    },
+    // Modify the proxy request to include query parameters
+    onProxyReq(proxyReq, req) {
+      if (req.query) {
+        const query = Object.keys(req.query)
+          .map((key) => `${key}=${encodeURIComponent(req.query[key])}`)
+          .join('&')
+        proxyReq.path += `?${query}`
+      }
+    },
+  })
+)
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
